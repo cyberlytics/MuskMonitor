3db582988be261b4bec8e239010c43c5d17d7995
Franziska Rubenbauer <86341509+Sammy159@users.noreply.github.com>
2024-12-05 15:33:45 +0100

    Anzeigen der Stimmung

--- sys-src/muskmonitor-frontend/src/components/timeSeriesDisplay.jsx
+++ sys-src/muskmonitor-frontend/src/components/timeSeriesDisplay.jsx
@@ -9,37 +9,60 @@ export default function TimeSeriesDisplay() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch("/tweets.json")
+    fetch("/tweets_with_classes.json")
       .then((response) => response.json())
-      .then((data) => setData(data))
+      .then((data) => {
+        const dataArray = Object.keys(data).map((key) => ({
+          text: key,
+          class: data[key],
+        }));
+        setData(dataArray);
+      })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   return (
     <VerticalTimeline>
-      {data.map((item, index) => (
-        <VerticalTimelineElement
-          key={index}
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
-          date="item.date"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          icon={
-            <img
-              src="/Tesla_Motors.png"
-              alt="Tesla Icon"
-              style={{ width: "100%", height: "100%" }}
-            ></img>
-          }
-        >
-          <h3 className="vertical-timeline-element-title">{item.title}</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            {item.subtitle}
-          </h4>
-          <p>{item.description}</p>
-        </VerticalTimelineElement>
-      ))}
+      {data.map((item, index) => {
+        let backgroundColor;
+        console.log(item);
+        switch (item.class.class) {
+          case "Positive":
+            backgroundColor = "green";
+            break;
+          case "Neutral":
+            backgroundColor = "gray";
+            break;
+          case "Negative":
+            backgroundColor = "red";
+            break;
+          default:
+            backgroundColor = "blue";
+        }
+
+        return (
+          <VerticalTimelineElement
+            key={index}
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: backgroundColor, color: "#000" }}
+            contentArrowStyle={{ borderRight: `7px solid ${backgroundColor}` }}
+            date={item.class.date}
+            iconStyle={{ background: "rgb(33, 150, 243)", color: "#000" }}
+            icon={
+              <img
+                src="/Tesla_Motors.png"
+                alt="Tesla Icon"
+                style={{ width: "100%", height: "100%" }}
+              ></img>
+            }
+          >
+            <h3 className="vertical-timeline-element-title">
+              {item.class.title}
+            </h3>
+            <p>{item.class.description}</p>
+          </VerticalTimelineElement>
+        );
+      })}
     </VerticalTimeline>
   );
 }
