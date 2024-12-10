a7caca84f0dd364b61035f3dd37c77fdaad88b76
Andreas Fillenberg <a.fillenberg@oth-aw.de>
2024-12-10 15:22:02 +0100

    Automatically get stock data and insert it into database

--- sys-src/muskmonitor-backend/webserver.py
+++ sys-src/muskmonitor-backend/webserver.py
@@ -5,6 +5,11 @@ from flask_pymongo import PyMongo
 from pymongo import MongoClient
 import bson.json_util
 import logging
+from flask_apscheduler import APScheduler
+import requests
+
+class FlaskAPSchedulerConfig:
+    SCHEDULER_API_ENABLED = True
 
 logger = logging.getLogger('Backend')
 logger.setLevel(logging.DEBUG)
@@ -22,11 +27,34 @@ weaviate = FlaskWeaviate(app)
 
 # app.config["MONGO_URI"] = "mongodb://root:root_password@stock-database:27017/stock_data?authSource=admin"
 # mongo = PyMongo(app)
-# logger.info("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
 # logger.info(mongo.cx.list_database_names())
 mongo = MongoClient("mongodb://root:root_password@stock-database:27017/")
 stock_database = mongo["stock_data"]
 tesla_stock = stock_database["tesla"]
+
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+API_KEY = "BJ22JP64AWPTKJN2"
+symbol = "TSLA"
+
+# Run this task at midnight everyday.
+@scheduler.task("cron", id="scrape_tesla_stock_daily", hour=0, minute=0)
+def scrape_tesla_stock_daily():
+    response = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&outputsize=full&apikey={API_KEY}")
+    data = response.json()["Time Series (Daily)"]
+    
+    for date, stock_data in data.items():
+        if tesla_stock.count_documents({"Datum": date}) == 0:
+            tesla_stock.insert_one({
+                "Datum": date,
+                "open": stock_data["1. open"],
+                "high": stock_data["2. high"],
+                "low": stock_data["3. low"],
+                "close": stock_data["4. close"],
+                "volume": stock_data["5. volume"],
+            })
 
 @app.route('/')
 def home():
